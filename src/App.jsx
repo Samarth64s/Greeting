95fb9f7b86d773557b5929af3f1ef360e7a678d1
Samarth64s <115937579+Samarth64s@users.noreply.github.com>
2023-07-13 10:36:36 +0530

    Greeting

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+
+function App(){
+    let currDate = new Date();
+    currDate = currDate.getHours();
+    let greeting ="";
+    const cssStyle={};
+
+    if(currDate>1 && currDate <12){
+        greeting = "Good Morning";
+        cssStyle.color= "blue";
+    }else if(currDate>=12 && currDate <20){
+        greeting = "Good Afternoon";
+        cssStyle.color = "orange"
+    }else if (currDate>=20 && currDate <=24){
+        greeting = "Good Night";
+        cssStyle.color = "Brown";
+    }
+
+    return(
+        <>
+        <div>
+            <h1> Hello sir, <span style={cssStyle}>{greeting}</span></h1>
+            </div>
+        </>
+    );
+}
+export default App;
